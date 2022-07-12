85e9d792a81e2bf759944a1996eca0a1f9cd211f
Anastasia <g.ankalashik@gmail.com>
2022-07-12 20:09:21 +0300

    coord button, added loading and icons

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,26 +1,52 @@
-function Location({ onLocation }) {
+import { useState } from "react";
+import Spinner from 'react-bootstrap/Spinner' //импортируется только спиннер, так лучше с большими библиотеками
+//import { Spinner } from 'react-bootstrap' // { } - импортируется всё, сейчас возьми спиннер
+import Button from 'react-bootstrap/Button'
 
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    }
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+}
+function Location({ onLocation }) {
+    const [loading, setLoading] = useState(false)
+    const [value, setValue] = useState()
+    const [error, setError] = useState()
 
     const onSuccess = (pos) => {
-            onLocation(pos.coords)
+        setLoading(false)
+        setValue(pos.coords)
+        onLocation(pos.coords)
     }
 
     const onError = (err) => {
+        setLoading(false)
         console.log(err)
     }
 
     const handleClick = () => {
+        setLoading(true)
         navigator.geolocation.getCurrentPosition(onSuccess, onError, options)
     }
 
     return(
         <div>
-            <button type="button" onClick={handleClick}>Определить координаты</button>
+            <Button
+                type="button"
+                onClick={handleClick}
+                variant="outline-primary"
+            >{value ? 'Координаты определены' : 'Определить координаты'}
+                {loading && (
+                    <Spinner
+                        className="ms-2"
+                        animation="border"
+                        size="small"
+                        variant="primary"
+                    />
+                )}
+                {value && <span className="ms-2">✅</span>}
+                {!value && !loading && <span className="ms-2">❌</span>}
+            </Button>
         </div>
     )
 }
